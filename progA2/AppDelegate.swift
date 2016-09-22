3e8f734bdfbefded07c4d8630a1c769c43dfa8ad
Ashley Midgley <amidgley@oucs1426.otago.ac.nz>
2016-09-22 13:47:26 +1200

    A

--- progA2/AppDelegate.swift
+++ progA2/AppDelegate.swift
@@ -7,15 +7,23 @@
 //
 
 import Cocoa
+import Quartz
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
-
+    @IBOutlet weak var ourPDF: PDFView!
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        let url = NSBundle.mainBundle().URLForResource("lect15", withExtension: "pdf")
+        let pdf = PDFDocument(URL: url)
+        //let pages: Int = pdf.pageCount()
+        //let text: String = pdf.string()
+        //let page1 = pdf.pageAtIndex(0)
+        ourPDF.setDocument(pdf)
+        //print("\(ourPDF.document) pages: \(pages)")
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {

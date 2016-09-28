d7b838d697e447ac8fd680fb7513e4bb0f8da7c5
Ashley Midgley <amidgley@oucs1445.otago.ac.nz>
2016-09-28 20:30:18 +1300

    Operations added

--- progA2/AppDelegate.swift
+++ progA2/AppDelegate.swift
@@ -14,15 +14,19 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
     @IBOutlet weak var ourPDF: PDFView!
-
+    @IBOutlet weak var currentPageNoLabel: NSTextField!
+    @IBOutlet weak var goToPageNoField: NSSearchField!
+    var pages = 1
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
         let url = NSBundle.mainBundle().URLForResource("myPDF", withExtension: "pdf")
-        //let pages: Int = pdf.pageCount()
-        //let text: String = pdf.string()
-        //let page1 = pdf.pageAtIndex(0)
         let pdf = PDFDocument(URL: url)
         ourPDF.setDocument(pdf)
+        pages = pdf.pageCount()
+        
+        let timer = NSTimer.scheduledTimerWithTimeInterval(0.2, target: self, selector: #selector(updateLabel), userInfo: nil, repeats: true)
+        timer.fire()
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
@@ -32,8 +36,41 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBAction func choosePDF(sender: NSButton) {
         NSWorkspace.sharedWorkspace().selectFile(nil, inFileViewerRootedAtPath: "/Home")
         //NSWorkspace.sharedWorkspace().activateFileViewerSelectingURLs(files)
-        
     }
 
+    @IBAction func nextPage(sender: NSButton) {
+        if(ourPDF.canGoToNextPage()){
+            ourPDF.goToNextPage(self)
+        }
+    }
+    
+    @IBAction func previousPage(sender: NSButton) {
+        if(ourPDF.canGoToPreviousPage()){
+            ourPDF.goToPreviousPage(self)
+        }
+    }
+    
+    @IBAction func zoomIn(sender: NSButton) {
+        if(ourPDF.canZoomIn()){
+            ourPDF.zoomIn(self)
+        }
+    }
+    
+    @IBAction func zoomOut(sender: NSButton) {
+        if(ourPDF.canZoomOut()){
+            ourPDF.zoomOut(self)
+        }
+    }
+    
+    @IBAction func choosePage(sender: NSSearchField) {
+        let pageNo = Int(goToPageNoField.stringValue)
+        if pageNo != nil && pageNo <= pages{
+            ourPDF.goToPage(ourPDF.document().pageAtIndex(pageNo!-1))
+        }
+    }
+    
+    func updateLabel(){
+        currentPageNoLabel.stringValue = "Page \(ourPDF.document().indexForPage(ourPDF.currentPage())+1) of \(pages)"
+    }
 }
 
